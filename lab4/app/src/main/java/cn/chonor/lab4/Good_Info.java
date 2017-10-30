b239f8c3f8404530090da90e085121465619657e
Chonor <caihonor@gmail.com>
2017-10-30 10:03:57 +0800

    lab4

---
+++ lab4/app/src/main/java/cn/chonor/lab4/Good_Info.java
@@ -0,0 +1,242 @@
+package cn.chonor.lab4;
+
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Chonor on 2017/10/21.
+ */
+
+public class Good_Info extends AppCompatActivity {
+    private int id;
+    private Data data=new Data();
+    private ImageView imageView;
+    private ImageView back;
+    private ImageView cart;
+    private ImageView star;
+    private TextView name;
+    private TextView price;
+    private TextView info;
+    private ListView list;
+    private ArrayList<Good> hit;
+    private boolean flag=false;
+    public static final String DYNAMICATION= "cn.chonor.lab4.dynamicreceiver";
+    private DynamicReceiver dynamicReceiver=new DynamicReceiver();
+    private IntentFilter dynamic_filter =new IntentFilter();
+    private String names;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.good_info);
+        init(); //初始化
+        broad_init();
+        get_set_data();//获取数据
+        init_listener();//设置监听
+    }
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        get_set_data();//获取数据
+        broad_init();
+    }
+    private void broad_init(){
+        dynamic_filter.addAction(DYNAMICATION);
+        registerReceiver(dynamicReceiver,dynamic_filter);
+    }
+    private void init(){ //初始化
+        String []hits ={"一键下单","分享商品","不感兴趣","查看更多商品促销信息"};
+        hit=new ArrayList<>();
+        for(int i=0;i<4;i++){//假装商品数据装进去
+            Good good = new Good();
+            good.setGoodName(hits[i]);
+            hit.add(good);
+        }
+        imageView=(ImageView)findViewById(R.id.imageView);
+        star = (ImageView)findViewById(R.id.imageView2);
+        cart = (ImageView)findViewById(R.id.imageView3);
+        back = (ImageView)findViewById(R.id.imageView4);
+        name = (TextView)findViewById(R.id.info_name);
+        price = (TextView)findViewById(R.id.info_price);
+        info = (TextView)findViewById(R.id.info_info);
+        list = (ListView)findViewById(R.id.myListView1);
+    }
+    private  void get_set_data(){
+        Bundle extras_main = getIntent().getBundleExtra("mainActivity"); //获取数据  主界面
+        Bundle extras_shop = getIntent().getBundleExtra("shoppingcart");//购物车
+        if (extras_main != null||extras_shop != null) {
+            if(extras_main != null) {  //主界面
+                flag = true;            //设置flag
+                id=extras_main.getInt("position"); //获得数据位置
+                ArrayList<Good>tmp = extras_main.getParcelableArrayList("data"); //获得数据
+                ArrayList<Good>tmp1 = extras_main.getParcelableArrayList("cart");
+                data.setGood_list(tmp);//设置数据
+                data.setCart_list(tmp1);
+            }
+            else{
+                flag=false;//购物车//设置flag
+                id=extras_shop.getInt("position");//获得数据位置
+                ArrayList<Good>tmp = extras_shop.getParcelableArrayList("data");//获得数据
+                ArrayList<Good>tmp1 = extras_shop.getParcelableArrayList("cart");
+                data.setGood_list(tmp);//设置数据
+                data.setCart_list(tmp1);
+            }
+            set_info();//设置界面
+        }
+    }
+    private void set_info(){
+        if(flag){//商品列表转跳时
+            names=data.getGood_list_index(id).getGoodName();
+            price.setText(data.getGood_list_index(id).getGoodPrice());
+            if (data.getGood_list_index(id).getStar() == 1)
+                star.setImageResource(R.mipmap.full_star);
+            else star.setImageResource(R.mipmap.empty_star);
+            info.setText(data.getGood_list_index(id).getGoodTypes() + " " + data.getGood_list_index(id).getGoodInfo());
+        }
+        else {//购物车列表转跳时
+            names=data.getCart_list_index(id).getGoodName();
+            price.setText(data.getCart_list_index(id).getGoodPrice());
+            if (data.getCart_list_index(id).getStar() == 1)
+                star.setImageResource(R.mipmap.full_star);
+            else star.setImageResource(R.mipmap.empty_star);
+            info.setText(data.getCart_list_index(id).getGoodTypes() + " " + data.getCart_list_index(id).getGoodInfo());
+        }
+        name.setText(names);
+        imageView.setImageResource(data.ID[data.map.get(names)]);
+        //设置下面4个items
+        MyAdapter myAdapter=new MyAdapter(this,hit,1);
+        list.setAdapter(myAdapter);
+    }
+    private void init_listener(){
+        star.setOnClickListener(new View.OnClickListener() { //收藏按钮监听
+            @Override
+            public void onClick(View view) {
+                if(flag) {//商品列表转跳时
+                    if (data.getGood_list_index(id).getStar() == 1) {//判断是否收藏
+                        star.setImageResource(R.mipmap.empty_star);
+                        Toast.makeText(Good_Info.this,"成功将"+data.getGood_list_index(id).getGoodName()+"取消收藏",Toast.LENGTH_SHORT).show();
+                    } else {
+                        star.setImageResource(R.mipmap.full_star);
+                        Toast.makeText(Good_Info.this,"成功将"+data.getGood_list_index(id).getGoodName()+"加入收藏",Toast.LENGTH_SHORT).show();
+                    }
+                    data.setGood_list_Stat(id); //同步购物车列表收藏
+                    for(int i=0;i<data.getCart_list().size();i++){
+                        if(data.getGood_list_index(id).getGoodName().equals(data.getCart_list_index(i).getGoodName())){
+                            data.setCart_list_Stat(i);
+                        }
+                    }
+                }else{//购物车列表转调试
+                    if (data.getCart_list_index(id).getStar() == 1) {//判断是否收藏
+                        star.setImageResource(R.mipmap.empty_star);
+                        Toast.makeText(Good_Info.this,"成功将"+data.getCart_list_index(id).getGoodName()+"取消收藏",Toast.LENGTH_SHORT).show();
+                    } else {
+                        star.setImageResource(R.mipmap.full_star);
+                        Toast.makeText(Good_Info.this,"成功将"+data.getCart_list_index(id).getGoodName()+"加入收藏",Toast.LENGTH_SHORT).show();
+                    }
+                    data.setCart_list_Stat(id); //同步商品列表收藏
+                    for(int i=0;i<data.getGood_list().size();i++){
+                        if(data.getCart_list_index(id).getGoodName().equals(data.getGood_list_index(i).getGoodName())){
+                            data.setGood_list_Stat(i);
+                        }
+                    }
+                }
+            }
+        });
+
+        cart.setOnClickListener(new View.OnClickListener() { //购物车按钮
+            @Override
+            public void onClick(View view) {
+                if(flag){
+                    int incart= -1;  //确定购物车里有没有
+                    for(int i=0;i<data.getCart_list().size();i++){
+                        if(data.getGood_list_index(id).getGoodName().equals(data.getCart_list_index(i).getGoodName()))
+                            incart=i;
+                    }
+                    if(incart== -1) //没有加入购物车
+                        data.addCart_list(data.getGood_list_index(id));
+                    else //有数量+1
+                        data.setCart_list_Cnt(incart,data.getCart_list_index(incart).getCnt()+1);
+                    Toast.makeText(Good_Info.this,"成功将"+data.getGood_list_index(id).getGoodName()+"加入购物车",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    data.setCart_list_Cnt(id,data.getCart_list_index(id).getCnt()+1);
+                    Toast.makeText(Good_Info.this,"成功将"+data.getCart_list_index(id).getGoodName()+"加入购物车",Toast.LENGTH_SHORT).show();
+                }
+                EventBus.getDefault().postSticky(new MessageEvent(data)); //发送数据
+                Intent iBroad=new Intent(DYNAMICATION);//发送广播
+                sendBroadcast(iBroad);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() { //返回按钮监听
+            @Override
+            public void onClick(View view) {  //数据返回
+                if(flag){
+                    Intent i = new Intent(Good_Info.this, MainActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelableArrayList("data", data.getGood_list());
+                    bundle.putParcelableArrayList("cart", data.getCart_list());
+                    i.putExtra("goodinfo", bundle);
+                    startActivity(i);
+                }
+                else {
+                    Intent i = new Intent(Good_Info.this, Shoppingcart.class);
+                    startActivity(i);
+                }
+            }
+        });
+    }
+    public class DynamicReceiver extends BroadcastReceiver {
+        public static final String DYNAMICATION= "cn.chonor.lab4.dynamicreceiver";
+        public DynamicReceiver(){}
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(DYNAMICATION)){
+                NotificationManager notifyManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+                Intent intent1 =new Intent (context,Shoppingcart.class);//点击事件和传输
+                PendingIntent pi = PendingIntent.getActivities(context, 0, new Intent[]{intent1}, PendingIntent.FLAG_CANCEL_CURRENT);
+                //实例化NotificationCompat.Builde并设置相关属性
+                Notification.Builder builder = new Notification.Builder(context)
+                        //设置小图标
+                        .setSmallIcon(data.ID[id])
+                        .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),data.ID[data.map.get(names)]))
+                        //设置通知标题
+                        .setTicker("TickerText:" + "您有新短消息，请注意查收！")
+                        .setContentTitle("马上下单")
+                        //设置通知内容
+                        .setContentText(names+"已添加到购物车")
+                        .setContentIntent(pi)
+                        .setAutoCancel(true)
+                        .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE)
+                        .setWhen(System.currentTimeMillis())
+                        .setPriority(Notification.PRIORITY_HIGH)
+                        .setVisibility(Notification.VISIBILITY_SECRET);
+                notifyManager.notify(0, builder.build());
+            }
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(dynamicReceiver);
+    }
+}
---
+++ lab4/app/src/main/java/cn/chonor/lab4/MainActivity.java
@@ -0,0 +1,222 @@
+package cn.chonor.lab4;
+
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.design.widget.FloatingActionButton;
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
+import jp.wasabeef.recyclerview.animators.OvershootInLeftAnimator;
+
+public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton floatingActionButton=null;
+    private Data data=new Data();
+    private RecyclerView mRecyclerView = null;
+    private CommonAdapter commonAdapter;
+    private ScaleInAnimationAdapter animationAdapter;
+    private Button button;
+    private Boolean flag;
+    protected RecyclerView.LayoutManager mLayoutManager;
+
+    private static final String STATICACTION = "cn.chonor.lab4.staticreceiver";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        init_listener();
+        broad_init();
+    }
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        back_data();
+    }
+
+    private void back_data(){
+        Bundle extras_info = getIntent().getBundleExtra("goodinfo");
+        Bundle extras_shop = getIntent().getBundleExtra("shoppingcart");//购物车
+        if (extras_info != null||extras_shop != null) {
+            if(extras_info != null) {  //主界面
+                ArrayList<Good>tmp = extras_info.getParcelableArrayList("data"); //获得数据
+                ArrayList<Good>tmp1 = extras_info.getParcelableArrayList("cart");
+                data.setGood_list(tmp);//设置数据
+                data.setCart_list(tmp1);
+            }
+            else{
+                ArrayList<Good>tmp = extras_shop.getParcelableArrayList("data");//获得数据
+                ArrayList<Good>tmp1 = extras_shop.getParcelableArrayList("cart");
+                data.setGood_list(tmp);//设置数据
+                data.setCart_list(tmp1);
+            }
+        }
+    }
+    private void init() {
+        flag=true;
+        button = (Button) findViewById(R.id.button);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton); //初始化按钮
+        mRecyclerView = (RecyclerView) findViewById(R.id.myRecyclerView); //初始化RecyclerView
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        commonAdapter = new CommonAdapter(MainActivity.this, R.layout.items, data.getGood_list(), flag);
+        animationAdapter = new ScaleInAnimationAdapter(commonAdapter);
+
+        animationAdapter.setDuration(1000); //设置开始动画
+        mRecyclerView.setAdapter(animationAdapter);//填充数据
+        mRecyclerView.setItemAnimator(new OvershootInLeftAnimator());
+        mRecyclerView.getItemAnimator().setRemoveDuration(300); //设置移除延时
+
+    }
+    private void init_listener(){
+        Adapter_listener();
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override //跳转至 购物车界面
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Shoppingcart.class);
+                Bundle bundle = new Bundle();//传输数据
+                bundle.putParcelableArrayList("data",data.getGood_list());
+                bundle.putParcelableArrayList("cart",data.getCart_list());
+                i.putExtra("mainActivity",bundle);
+                startActivity(i);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(flag){
+                    flag=false;
+                }else{
+                    flag=true;
+                }
+                change();
+            }
+        });
+    }
+    private void Adapter_listener(){
+        commonAdapter.setOnItemClickListener(new CommonAdapter.OnItemClickListener(){
+            @Override
+            public void onClick(int position){
+                if(position >= 0) {
+                    Intent i = new Intent(MainActivity.this, Good_Info.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("position", position);
+                    bundle.putParcelableArrayList("data", data.getGood_list());
+                    bundle.putParcelableArrayList("cart", data.getCart_list());
+                    i.putExtra("mainActivity", bundle);
+                    startActivity(i);
+                }
+            }
+            @Override
+            public void onLongClick(int position) {
+                if (position >= 0) {
+                    data.removeGood_list_index(position);
+                    animationAdapter.notifyItemRemoved(position);
+                    mRecyclerView.setItemViewCacheSize(data.getGood_list().size());
+                    Toast.makeText(MainActivity.this, "移除了第"+String.valueOf(position)+"件商品", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private void change(){
+        int scrollPosition = 0;
+        // If a layout manager has already been set, get current scroll position.
+        if (mRecyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }//布局切换
+        if(flag) {
+            mLayoutManager = new LinearLayoutManager(this);
+            commonAdapter = new CommonAdapter(MainActivity.this, R.layout.items, data.getGood_list(), flag);
+        }else{
+            mLayoutManager = new GridLayoutManager(this, 2);
+            commonAdapter = new CommonAdapter(MainActivity.this, R.layout.item2, data.getGood_list(), flag);
+        }
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        animationAdapter = new ScaleInAnimationAdapter(commonAdapter);
+        mRecyclerView.setAdapter(animationAdapter);//填充数据
+        Adapter_listener();//建立监听
+        mRecyclerView.scrollToPosition(scrollPosition);
+    }
+    private void broad_init(){
+        Random random=new Random();
+        int i=random.nextInt(10);
+        Intent iBroad=new Intent(STATICACTION);
+        Bundle bundle = new Bundle();
+        bundle.putInt("position", i);
+        iBroad.putExtra("mainActivity",bundle);
+        sendBroadcast(iBroad);
+    }
+
+    public static class StaticReceiver extends BroadcastReceiver {
+        public StaticReceiver(){}
+        @Override		//静态广播接收器执行的方法
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(STATICACTION)) {
+                Bundle extras = intent.getBundleExtra("mainActivity");
+                Data data=new Data();
+                int id=extras.getInt("position"); //获得数据位置
+                Bitmap bm=BitmapFactory.decodeResource(context.getResources(),data.ID[id]);
+                String name=data.getGood_list_index(id).getGoodName();
+                String price=data.getGood_list_index(id).getGoodPrice();
+
+                NotificationManager notifyManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+                Intent intent1 =new Intent (context,Good_Info.class); //点击事件和传输
+                Bundle bundle = new Bundle();
+                bundle.putInt("position", id);
+                bundle.putParcelableArrayList("data",data.getGood_list());
+                bundle.putParcelableArrayList("cart", data.getCart_list());
+                intent1.putExtra("mainActivity", bundle);
+                PendingIntent pi = PendingIntent.getActivities(context, 0, new Intent[]{intent1}, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                //实例化NotificationCompat.Builde并设置相关属性
+                Notification.Builder builder = new Notification.Builder(context)
+                        //设置小图标
+                        .setSmallIcon(data.ID[id])
+                        .setLargeIcon(bm)
+                        //设置通知标题
+                        .setTicker("TickerText:" + "您有新短消息，请注意查收！")
+                        .setContentTitle("新商品热卖")
+                        //设置通知内容
+                        .setContentText(name+"仅售"+price+"!")
+                        .setContentIntent(pi)
+                        .setAutoCancel(true)
+                        .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE)
+                        .setWhen(System.currentTimeMillis())
+                        .setPriority(Notification.PRIORITY_HIGH)
+                        .setVisibility(Notification.VISIBILITY_SECRET);
+                notifyManager.notify(0, builder.build());
+            }
+        }
+    }
+}
